13ca6401773e2b54c0d95bd73cec35cd0dd6e7d2
alanyumbo123 <aj.yumbog@uea.edu.ec>
2026-03-08 20:30:01 -0500

    libro.py

--- modelos/libro.py
+++ modelos/libro.py
@@ -1,1 +1,19 @@
+# Modelo que representa un libro en la biblioteca
 
+class Libro:
+
+    def __init__(self, titulo, autor, categoria, isbn):
+        # título y autor almacenados como tupla (requisito)
+        self.info = (titulo, autor)
+
+        self.categoria = categoria
+        self.isbn = isbn
+
+    def obtener_titulo(self):
+        return self.info[0]
+
+    def obtener_autor(self):
+        return self.info[1]
+
+    def mostrar_info(self):
+        return f"Título: {self.obtener_titulo()} | Autor: {self.obtener_autor()} | Categoría: {self.categoria} | ISBN: {self.isbn}"
